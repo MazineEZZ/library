bb2e28fd06d9477895dc1a4657506f90d62671b1
MazineEZZ <mazinewolf2007@gmail.com>
2024-03-12 15:38:26 +0000

    Styled the cards and started working on the js script file

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const myLibrary = [];
+const myLibrary = {
+    "The Hobbit": {
+      author: "J. R. R. Tolkien",
+      pages: 295
+    },
+    "Atomic habits": {
+      author: "James Clear",
+      pages: 320
+    }
+};
+
+const bookWrapper = document.getElementById("bookWrapper");
 
 function Book() {
   // the constructor...
@@ -9,5 +20,20 @@ function addBookToLibrary() {
 }
 
 function displayBooks() {
-  
+  bookDiv = document.createElement("div");
+  bookDiv.classList.add("book")
+  bookTitle = document.createElement("div");
+  bookTitle.classList.add("book-title");
+  bookDetails = document.createElement("div");
+  bookDetails.classList.add("book-details")
+  bookAuthor = document.createElement("div");
+  bookAuthor.classList.add("author")
+  bookPages = document.createElement("div");
+  bookPages.classList.add("pages")
+
+  for (let book in myLibrary) {
+    bookTitle.textContext = book;
+    myLibrary[book].author;
+    myLibrary[book].pages
+  }
 }
